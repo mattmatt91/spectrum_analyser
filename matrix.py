fdcc2137e611fbf96ccbe48793e7ed55eef2caed
mattmatt91 <matthias_muhr@web.de>
2022-11-01 18:19:56 +0100

    requirements.txt

--- matrix.py
+++ matrix.py
@@ -13,20 +13,20 @@ DELAY = 0.1
 spi = board.SPI()
 length = 16
 
+
 class Matrix():
-    def __init__(self):        
+    def __init__(self):
         self.pixels = neopixel.NeoPixel_SPI(spi,
-                               length*length,
-                                brightness=0.01,
-                               pixel_order=PIXEL_ORDER,
-                               auto_write=False)
-        
-                 
+                                            length*length,
+                                            brightness=0.01,
+                                            pixel_order=PIXEL_ORDER,
+                                            auto_write=False)
+
     def draw(self, x, y, color):
-        if x%2 == 0:
-            index = length  * (length-x) - (y+1)
+        if x % 2 == 0:
+            index = length * (length-x) - (y+1)
         else:
-            index = length  * (length-(x+1))+y
+            index = length * (length-(x+1))+y
         # index = get_index(x,y)
         self.pixels[index] = color
 
@@ -34,36 +34,44 @@ class Matrix():
         self.pixels.fill(0)
 
 
-def get_sine(x, freq,amp):
-    #return int(((math.sin((x+time.time()))+1)/2)*length/6)
+def get_sine(x, freq, amp):
     v = ((math.sin(((time.time())*freq))+1)/2)*x
-    v *=amp
-    
+    v *= amp
     return int(v)
 
-def get_sine2(cnt,x, freq):
-    #return int(((math.sin((x+time.time()))+1)/2)*length/6)
-    return int(((math.sin(((cnt)*freq))+1)/2)*x)
 
 def wheel(pos):
-        if pos < 0 or pos > 255:
-            r = g = b = 0
-        elif pos < 85:
-            r = int(pos * 3)
-            g = int(255 - pos * 3)
-            b = 0
-        elif pos < 170:
-            pos -= 85
-            r = int(255 - pos * 3)
-            g = 0
-            b = 0#int(pos * 3)
-        else:
-            pos -= 170
-            r = 0
-            g = int(pos * 3)
-            b = int(255 - pos * 3)
-        return [r, g, b] # if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
-        return (r, g, b) # if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = 0  # int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    # if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+    return [r, g, b]
+    return (r, g, b)  # if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+def draw_sine(self, cnt):
+    for x in range(length):
+        color = wheel(((cnt+length//2)*length) % 255)
+        ymax = get_sine(length, 32, 0.5) + length//3
+        for y in range(ymax):
+            matrix.draw(x, y, color)
+        for y in range(length-ymax):
+            matrix.draw(x, y+ymax, color)
+        matrix.draw(x, ymax, wheel((cnt+length) % 255))
+        matrix.pixels.show()
 
 
 if __name__ == '__main__':
@@ -72,16 +80,5 @@ if __name__ == '__main__':
     cnt = 0
     while True:
         # matrix.clear()
-        for x in range(length):
-            color = wheel(((cnt+length//2)*length)%255)
-            ymax = get_sine(length,32, 0.5)+ length//3
-            for y in range(ymax):
-                matrix.draw(x,y, color) 
-            for y in range(length-ymax):
-                matrix.draw(x,y+ymax, color) 
-            matrix.draw(x,ymax, wheel((cnt+length)%255)) 
-
-            matrix.pixels.show()
+        matrix.draw_sine(cnt)
         cnt += 1
-    
-
