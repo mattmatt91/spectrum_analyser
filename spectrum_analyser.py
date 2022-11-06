bc63028e66686322facdbd6371a7cd4b351f53f3
mattmatt91 <matthias_muhr@web.de>
2022-11-06 17:38:12 +0100

    updating UI

--- spectrum_analyser.py
+++ spectrum_analyser.py
@@ -6,25 +6,12 @@ from math import log
 from matrix import Matrix, wheel
 from animated_figs import Animations
 
-CHUNK = 1024 # audio resolution
+CHUNK = 1024  # audio resolution
 FORMAT = pa.paInt16
 CHANNELS = 1
 RATE = 44100  # in Hz
-BANDS = 16 # number of pixel cols
-DEVICE_INDEX = 2 # audio interface
-
-BRIGHTNESS = 0.1
-FREQ_AREA = 20  # win size of raw data --> change in UI
-WIN_SIZE = FREQ_AREA/BANDS # size of array for one band at matrix
-SMOOTH = 3  # high value is slow fall --> change in UI
-FALLDOWN = 7  # high value is slow fall --> change in UI
-FADESPEED = 5  # color change speed, high value is lsow speed --> change in UI
-RAINBOW = 4  # 255//BANDS  # gradient of colors for x axis --> change in UI
-YRAINBOW = 20 # 255//BANDS  # gradient of colors for y axis --> change in UI
-SYM = True # --> change in UI
-MAXDOT = True# draw max dot  --> change in UI
-BLACKSPEC = False # --> change in UI
-CENTER = False # when sym is true, drw center or borders --> change in UI
+BANDS = 16  # number of pixel cols
+DEVICE_INDEX = 2  # audio interface
 
 
 def between0and1(val):
@@ -37,10 +24,22 @@ def between0and1(val):
 
 
 def cut_sub_array(dataFFT, band):
-    start = int(WIN_SIZE*band)
-    stop = int(WIN_SIZE*(band+1))
+    start = int(Frame.WIN_SIZE*band)
+    stop = int(Frame.WIN_SIZE*(band+1))
     sub_arr = dataFFT[start:stop]
     return sub_arr
+
+# mapping values between 0 and 100 to range
+
+
+def mapping_to_range(val, min_o=0, max_o=1, dtype=float):
+    if dtype == bool:
+        return bool(round(int(val)/100, 0))
+    val = int(val)
+    m = (max_o - min_o)/100
+    b = min_o
+    mapped_value = m * val + b
+    return dtype(mapped_value)
 
 
 class NeoPixelMatrix:
@@ -49,34 +48,43 @@ class NeoPixelMatrix:
         self.cnt = 0
 
     def render_spec(self, old_values, max_values):
-        col_val = (self.cnt//FADESPEED)
+        col_val = (self.cnt//Frame.FADESPEED)
         for x in range(BANDS):
-            col_val += RAINBOW
-            if SYM:   
+            col_val += Frame.RAINBOW
+            if Frame.SYM:
                 for i in range(old_values[x]//2):
-                    color = [0,0,0] if BLACKSPEC else wheel((col_val+YRAINBOW*i) % 255)
-                    if CENTER:
-                        self.matrix.draw_pixel(x, i+BANDS//2-1, color, invert=True, rot_y=True)
-                        self.matrix.draw_pixel(x, BANDS//2-i, color, invert=True, rot_y=True)
+                    color = [0, 0, 0] if Frame.BLACKSPEC else wheel(
+                        (col_val+Frame.YRAINBOW*i) % 255)
+                    if Frame.CENTER:
+                        self.matrix.draw_pixel(
+                            x, i+BANDS//2-1, color, invert=True, rot_y=True)
+                        self.matrix.draw_pixel(
+                            x, BANDS//2-i, color, invert=True, rot_y=True)
                     else:
-                        self.matrix.draw_pixel(x, i, color, invert=True, rot_y=True)
-                        self.matrix.draw_pixel(x, BANDS-i-1, color, invert=True, rot_y=True)
-                if MAXDOT:
-                    if CENTER:
-                        self.matrix.draw_pixel(x, max_values[x]//2+BANDS//2-1, [255, 0, 0], invert=True, rot_y=True)
-                        self.matrix.draw_pixel(x, BANDS//2-max_values[x]//2, [255, 0, 0], invert=True, rot_y=True)
+                        self.matrix.draw_pixel(
+                            x, i, color, invert=True, rot_y=True)
+                        self.matrix.draw_pixel(
+                            x, BANDS-i-1, color, invert=True, rot_y=True)
+                if Frame.MAXDOT:
+                    if Frame.CENTER:
+                        self.matrix.draw_pixel(
+                            x, max_values[x]//2+BANDS//2-1, [255, 0, 0], invert=True, rot_y=True)
+                        self.matrix.draw_pixel(
+                            x, BANDS//2-max_values[x]//2, [255, 0, 0], invert=True, rot_y=True)
                     else:
-                        self.matrix.draw_pixel(x, max_values[x]//2, [255, 0, 0], invert=True, rot_y=True)
-                        self.matrix.draw_pixel(x, BANDS-max_values[x]//2 -1, [255, 0, 0], invert=True, rot_y=True)
+                        self.matrix.draw_pixel(
+                            x, max_values[x]//2, [255, 0, 0], invert=True, rot_y=True)
+                        self.matrix.draw_pixel(
+                            x, BANDS-max_values[x]//2 - 1, [255, 0, 0], invert=True, rot_y=True)
 
-            if not SYM:
+            if not Frame.SYM:
                 for i in range(old_values[x]):
-                    color = [0,0,0] if BLACKSPEC else wheel((col_val+ YRAINBOW*i)% 255) 
-                    self.matrix.draw_pixel(x, i, color , rot_x=True, rot_y=True)
-                if MAXDOT:
-                    self.matrix.draw_pixel(x, max_values[x], [255, 0, 0], rot_x=True, rot_y=True)
-
-
+                    color = [0, 0, 0] if Frame.BLACKSPEC else wheel(
+                        (col_val + Frame.YRAINBOW*i) % 255)
+                    self.matrix.draw_pixel(x, i, color, rot_x=True, rot_y=True)
+                if Frame.MAXDOT:
+                    self.matrix.draw_pixel(
+                        x, max_values[x], [255, 0, 0], rot_x=True, rot_y=True)
 
     def show(self):
         self.cnt += 1
@@ -85,12 +93,10 @@ class NeoPixelMatrix:
     def clear(self):
         self.matrix.clear()
 
-
     def render_animation(self, dots):
         for dot in dots:
-            self.matrix.draw_pixel(int(dot[0]), int(dot[1]), dot[2], rot_x=True, rot_y=True)
-        
-
+            self.matrix.draw_pixel(int(dot[0]), int(
+                dot[1]), dot[2], rot_x=True, rot_y=True)
 
 
 class Visualization():
@@ -98,12 +104,12 @@ class Visualization():
         self.old_vals = [0 for _ in range(16)]
         self.max_vals = [0 for _ in range(16)]
 
-    def update_max(self,cnt, new_data):
+    def update_max(self, cnt, new_data):
         for new, old, i in zip(new_data, self.max_vals, range(BANDS)):
             if new >= old:
                 self.max_vals[i] = new
             else:
-                if cnt % FALLDOWN == 0:
+                if cnt % Frame.FALLDOWN == 0:
                     self.max_vals[i] = old - 1 if old > 0 else old
 
     def update_last(self, cnt,  new_data):
@@ -111,10 +117,10 @@ class Visualization():
             if new >= old:
                 self.old_vals[i] = new
             else:
-                if cnt % SMOOTH == 0:
+                if cnt % Frame.SMOOTH == 0:
                     self.old_vals[i] = old - 1 if old > 0 else old
 
-    def update(self,cnt, data):
+    def update(self, cnt, data):
         self.update_max(cnt, data)
         self.update_last(cnt, data)
 
@@ -149,7 +155,6 @@ class Stream(object):
         dataFFT = np.abs(np.fft.fft(dataInt))*2/(11000*CHUNK)
         return dataInt, dataFFT
 
-
     def map_ln(self, val):
         val = 0 if val <= 0 else val
         if val == 0:
@@ -165,13 +170,26 @@ class Stream(object):
             if val > self.max_val:
                 self.max_val = val
             val = val/self.max_val
-            val = self.map_ln(val) # fit to signal
+            val = self.map_ln(val)  # fit to signal
             normalised.append(int(val*(BANDS)))
         return normalised
 
 
-
 class Frame():
+    BRIGHTNESS = 0.1
+
+    FREQ_AREA = 20  # win size of raw data --> change in UI
+    WIN_SIZE = FREQ_AREA/BANDS  # size of array for one band at matrix
+    SMOOTH = 3  # high value is slow fall --> change in UI
+    FALLDOWN = 7  # high value is slow fall --> change in UI
+    FADESPEED = 10  # color change speed, high value is lsow speed --> change in UI
+    RAINBOW = 10  # 255//BANDS  # gradient of colors for x axis --> change in UI
+    YRAINBOW = 20  # 255//BANDS  # gradient of colors for y axis --> change in UI
+    SYM = False  # --> change in UI
+    MAXDOT = True  # draw max dot  --> change in UI
+    BLACKSPEC = False  # --> change in UI
+    CENTER = True  # when sym is true, drw center or borders --> change in UI
+
     def __init__(self) -> None:
         self.stream = Stream()
         self.visualization = Visualization()
@@ -179,57 +197,70 @@ class Frame():
         self.animations = Animations()
 
     def set_brightness(self, val):
-        print(val)
-        val = int(val)/100
-        print(val)
+        val = mapping_to_range(val, 0, 1, float)
+        self.neopixelmatrix.matrix.pixels.brightness = val
+        return val
 
-        self.neopixelmatrix.matrix.pixels.brightness  = val
-    
+    @classmethod
+    def set_freq_area(cls, val):
+        val = mapping_to_range(val, BANDS, 200, int)
+        cls.FREQ_AREA = val
+        cls.WIN_SIZE = Frame.FREQ_AREA/BANDS
+        return val
 
+    @classmethod
+    def set_smooth(cls, val):
+        val = mapping_to_range(val, 1, 10, int)
+        cls.SMOOTH = val
+        return val
 
-    def set_freq_area(self, val):
-        # val = 
-        global FREQ_AREA
-        FREQ_AREA = val 
-        global WIN_SIZE
-        WIN_SIZE = FREQ_AREA/BANDS 
-    
-    def set_smooth(self, val):
-        global SMOOTH 
-        SMOOTH = val 
+    @classmethod
+    def set_falldown(cls, val):
+        val = mapping_to_range(val, cls.SMOOTH, 15, int)
+        cls.FALLDOWN = val
+        return val
 
-    def set_falldown(self, val):
-        global FALLDOWN
-        FALLDOWN = val 
-        
-    def set_fadespeed(self, val):
-        global FADESPEED
-        FADESPEED = val
-         
-    def set_rainbow(self, val):
-        global RAINBOW
-        RAINBOW = val
-    
-    def set_yrainbow(self, val):
-        global YRAINBOW
-        YRAINBOW = val
-    
-    def set_sym(self, val):
-        global SYM
-        SYM = val
-    
-    def set_maxdot(self, val):
-        global MAXDOT
-        MAXDOT = val
-    
-    def set_blackspec(self, val):
-        global BLACKSPEC
-        BLACKSPEC = val
+    @classmethod
+    def set_fadespeed(cls, val):
+        val = mapping_to_range(val, 1, 20, int)
+        cls.FADESPEED = val
+        return val
 
-    def set_center(self, val):
-        global CENTER 
-        CENTER = val
+    @classmethod
+    def set_rainbow(cls, val):
+        val = mapping_to_range(val, 0, 30, int)
+        cls.RAINBOW = val
+        return val
 
+    @classmethod
+    def set_yrainbow(cls, val):
+        val = mapping_to_range(val, 0, 30, int)
+        cls.YRAINBOW = val
+        return val
+
+    @classmethod
+    def set_sym(cls, val):
+        val = mapping_to_range(val, 0, 1, bool)
+        cls.SYM = val
+        return val
+
+    @classmethod
+    def set_maxdot(cls, val):
+        val = mapping_to_range(val, 0, 1, bool)
+        cls.MAXDOT = val
+        return val
+
+    @classmethod
+    def set_blackspec(cls, val):
+        val = mapping_to_range(val, 0, 1, bool)
+        cls.BLACKSPEC = val
+        return val
+
+    @classmethod
+    def set_center(cls, val):
+        val = mapping_to_range(val, 0, 1, bool)
+        cls.CENTER = val
+        return val
 
     def update(self):
         while True:
@@ -241,15 +272,17 @@ class Frame():
 
             # update visualization
             self.visualization.update(self.neopixelmatrix.cnt, data)
-
             # render to led matrix
-
             self.neopixelmatrix.clear()
             # self.neopixelmatrix.render_animation(self.animations.get_animation(func='onair', beat=data[2]))
-            # self.neopixelmatrix.render_animation(self.animations.get_animation(func='random', beat=data[1]))
+            # self.neopixelmatrix.render_animation(self.animations.get_animation(func='modulo', beat=data[2]))
+            # self.neopixelmatrix.render_animation(
+            # self.animations.get_animation(func='random', beat=data[1]))
             # self.neopixelmatrix.render_animation(self.animations.get_animation( func='men', beat=data[0]))
-            self.neopixelmatrix.render_spec(self.visualization.old_vals, self.visualization.max_vals)
+            self.neopixelmatrix.render_spec(
+                self.visualization.old_vals, self.visualization.max_vals)
             self.neopixelmatrix.show()
+
 
 if __name__ == '__main__':
     frame = Frame()
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-@app.route('/center/<center>', methods=['POST', 'GET'])
-def setcenter(center):
-	# function 
-	print ("center: {}".format(center))
-	return jsonify(result=center)
+# mapping values between 0 and 100 to range
+def mapping_to_range(val, min_o, max_o, dtype):
+	m = (max_o - min_o)/100
+	b = min_o
+	mapped_value = m * val + b
+	return dtype(mapped_value)
+
+
+print(bool(round(0.2, 0)), bool(round(0.9, 0)))
